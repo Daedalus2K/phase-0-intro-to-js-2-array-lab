dc6c072c8c9862e3cd011d62e1f9867a4c1c4751
Daedalus2K <apolloswatered@gmail.com>
2023-01-22 23:21:16 -0500

    commit tree ddd3112c06977119d966d1b4cfc5b53d32d24740

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){cats.push("Ralph")}
+
+function destructivelyPrependCat(){cats.unshift("Bob")}
+
+function destructivelyRemoveLastCat(){cats.pop("Garfield")}
+
+function destructivelyRemoveFirstCat(){cats.shift("Milo")}
+
+function appendCat(){
+    let cats = ["Milo", "Otis", "Garfield"]
+    const cats2 = [...cats,"Broom"];
+    return cats2
+}
+function prependCat(){
+    let cats = ["Milo", "Otis", "Garfield"]
+    const cats3 = ["Arnold",...cats]
+    return cats3
+} 
+
+function removeLastCat(){
+    let cats = ["Milo", "Otis", "Garfield"]
+    const cats4 = cats.slice(0,2)
+    return cats4
+}
+
+function removeFirstCat(){
+    let cats = ["Milo", "Otis", "Garfield"]
+    const cats5 = cats.slice(1)
+    return cats5
+}console.log(removeFirstCat())
+    
+
